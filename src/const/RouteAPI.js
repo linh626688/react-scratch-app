eb4861aaa9ac1e5ae34fdd54f01561a2f7219cc2
linhdt <linh626688@gmail.com>
2019-11-22 13:28:07 +0700

    change API

--- src/const/RouteAPI.js
+++ src/const/RouteAPI.js
@@ -1,5 +1,5 @@
-// export const API = `http://18.220.225.250:8080`;
-export const API = `http://localhost:8080`;
+export const API = `http://18.220.225.250:8080`;
+// export const API = `http://localhost:8080`;
 export const API_ROUTES = {
   LOGIN: `${API}/auth/login`,
   SIGN_UP: `${API}/auth/signup`,
